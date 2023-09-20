0fd49b2b0082c4d9d442758f575f8cfef5f94667
Jeremy Tejo Fernandez <jeremy.tejo@usach.cl>
2023-09-20 20:40:10 -0300

    Inicio de Proyecto

---
+++ ProyectoTingeso_evaluacion1/src/main/java/com/example/ProyectoTingeso_evaluacion1/ProyectoTingesoEvaluacion1Application.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoTingeso_evaluacion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTingesoEvaluacion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTingesoEvaluacion1Application.class, args);
+	}
+
+}
---
+++ ProyectoTingeso_evaluacion1/src/test/java/com/example/ProyectoTingeso_evaluacion1/ProyectoTingesoEvaluacion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoTingeso_evaluacion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTingesoEvaluacion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
